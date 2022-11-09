f3d2628dfdcee4fbbb26283f4bc171f1348ce928
Павел Шевель <pawel.pavlenti@yandex.ru>
2022-11-09 19:02:54 +0300

    ревью

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -75,7 +75,7 @@ else:
             'NAME': os.getenv('DB_NAME', default='postgres'),
             'USER': os.getenv('POSTGRES_USER', default='postgres'),
             'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
-            'HOST': os.getenv('DB_HOST', default='localhost'),
+            'HOST': os.getenv('DB_HOST', default='db'),
             'PORT': os.getenv('DB_PORT', default='5432')
         }
     }
